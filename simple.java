85d1d8b3e971bc6cde40c12b0367f1319a134195
hirvesarika21 <sarikahirve21@gmail.com>
2019-07-24 16:06:33 +0530

    mkmk

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello india ggfdgd fdfdlfmldmlm");  
+     System.out.println("Hello india ggfdgd fdfdlfmldmlm hhh");  
     }  
 }  
